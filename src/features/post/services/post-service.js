35fcb355976e00603f9c44601018c00dc14a663e
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-06-04 17:02:53 +0100

    feat: add services for blog post API calls

---
+++ src/features/post/services/post-service.js
@@ -0,0 +1,41 @@
+import api from "../../../adapters/api.js";
+
+const basePath = "/posts";
+
+const getPosts = async () => {
+  const res = await api.get(basePath);
+  return res.data;
+};
+
+const getPostById = async (postId) => {
+  const res = await api.get(`${basePath}/${postId}`);
+  return res.data;
+};
+
+const createPost = async ({ title, content, authorId, categories }) => {
+  const res = await api.post(basePath, {
+    title,
+    content,
+    authorId,
+    categories,
+  });
+
+  return res.data;
+};
+
+const updatePost = async ({ postId, title, content, categories }) => {
+  const res = await api.put(`${basePath}/${postId}`, {
+    title,
+    content,
+    categories,
+  });
+
+  return res.data;
+};
+
+const deletePost = async (postId) => {
+  const res = await api.delete(`${basePath}/${postId}`);
+  return res.data;
+};
+
+export { getPosts, getPostById, createPost, updatePost, deletePost };
