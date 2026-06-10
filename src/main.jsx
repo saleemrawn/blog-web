bc1b78f0d1a1484d1011c9799f2ea25d6163d29f
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-06-10 11:02:10 +0100

    feat: add AuthProvider and RouterProvider with DataRouter paths
    - add AuthProvider to handle authentication, authorisation
    - add RouterProvider and pass DataRouter application paths for root, register, login and posts

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { RegisterForm, LoginForm, AuthProvider } from "./features/auth";
+import { Post, PostForm, PostList } from "./features/post";
 import "./index.css";
 import App from "./App.jsx";
+import Home from "./pages/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "register", element: <RegisterForm /> },
+      { path: "login", element: <LoginForm /> },
+      {
+        path: "posts",
+        children: [
+          { index: true, element: <PostList /> },
+          {
+            path: ":postId",
+            element: <Post />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 );
