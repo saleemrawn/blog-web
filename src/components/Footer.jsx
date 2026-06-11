90a4b1060db4519a8b448f775fcb775fe18e207d
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-06-11 15:10:06 +0100

    refactor: simplify footer to only display copyright text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,58 +1,16 @@
-import { Link } from "react-router";
-import { useAuthContext } from "../features/auth/context/AuthContext";
-import { useLogout } from "../features/auth/hooks/useAuth";
-import { Box, Flex, Text, Link as RadixLink } from "@radix-ui/themes";
+import { Box, Text } from "@radix-ui/themes";
 
 const Copyright = () => {
   const date = new Date();
   return <Text>&#169; {date.getFullYear()} The Blog</Text>;
 };
 
-const FooterLinks = () => {
-  const { isLoggedIn, isLoading } = useAuthContext();
-  const { logout } = useLogout();
-
-  if (isLoggedIn) {
-    return (
-      <RadixLink weight={"bold"} color="gray" highContrast asChild>
-        <Link className="nav-link" onClick={logout}>
-          Logout
-        </Link>
-      </RadixLink>
-    );
-  }
-
-  return (
-    <Flex gap={"4"}>
-      <Box>
-        <RadixLink weight={"bold"} color="gray" highContrast asChild>
-          <Link to={"/login"} className="nav-link">
-            Login
-          </Link>
-        </RadixLink>
-      </Box>
-      <Box>
-        <RadixLink weight={"bold"} color="gray" highContrast asChild>
-          <Link to={"/sign-up"} className="nav-link">
-            Sign-Up
-          </Link>
-        </RadixLink>
-      </Box>
-    </Flex>
-  );
-};
-
 export const Footer = () => {
   return (
     <footer>
-      <Flex justify={"between"} py={"4"} align={"center"}>
-        <Box>
-          <Copyright />
-        </Box>
-        <Box>
-          <FooterLinks />
-        </Box>
-      </Flex>
+      <Box py={"4"}>
+        <Copyright />
+      </Box>
     </footer>
   );
 };
