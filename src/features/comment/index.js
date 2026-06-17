e12c1e644ee75890c8fea5fb1f1f462e60a57c9d
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-06-17 19:16:46 +0100

    feat: add root export file for comment components, hooks, services

---
+++ src/features/comment/index.js
@@ -0,0 +1,4 @@
+export * from "../comment/hooks/useComment";
+export { CommentsList } from "./components/CommentList";
+export { CommentCount } from "./components/CommentCount";
+export { CommentForm } from "./components/CommentForm";
