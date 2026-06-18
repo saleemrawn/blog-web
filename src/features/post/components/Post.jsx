1680c3b94e8009a94dc6a2c0f7d1a2785037ac11
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-06-18 16:55:31 +0100

    feat: add Post component

---
+++ src/features/post/components/Post.jsx
@@ -0,0 +1,64 @@
+import { useEffect } from "react";
+import { useParams } from "react-router";
+import { useGetPostById } from "../";
+import { useGetComments, CommentForm, CommentsList } from "../../comment";
+import { ErrorMessage } from "../../../components/ErrorMessage";
+import {
+  Box,
+  Container,
+  Heading,
+  Skeleton,
+  Spinner,
+  Text,
+} from "@radix-ui/themes";
+
+export const Post = () => {
+  const { postId } = useParams();
+  const {
+    post,
+    isLoading: isPostLoading,
+    error: postError,
+  } = useGetPostById(postId);
+  const {
+    comments,
+    isLoading: isCommentsLoading,
+    error: commentsError,
+    refetch,
+  } = useGetComments(postId);
+
+  const isLoading = isPostLoading || isCommentsLoading;
+
+  if (postError) {
+    return <ErrorMessage error={postError} />;
+  }
+
+  return (
+    <>
+      <Box>
+        <Container size={"4"}>
+          <Box mb={"6"}>
+            <Skeleton loading={isLoading}>
+              <Heading size={{ initial: "6", sm: "8" }} mb="6">
+                {post?.title}
+              </Heading>
+            </Skeleton>
+
+            <Skeleton loading={isLoading}>
+              <Text>{post?.content}</Text>
+            </Skeleton>
+          </Box>
+
+          <Skeleton loading={isLoading}>
+            <Box>
+              <CommentsList
+                comments={comments}
+                isLoading={isLoading}
+                onCommentCreated={refetch}
+              />
+            </Box>
+          </Skeleton>
+        </Container>
+      </Box>
+    </>
+  );
+};
