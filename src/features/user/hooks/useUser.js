48b769270ec19016a9c8c2ca7faaaef7c5ced944
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-06-22 16:55:02 +0100

    feat: add useCreateUser custom hook

---
+++ src/features/user/hooks/useUser.js
@@ -0,0 +1,28 @@
+import * as userService from "../services/userService";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
+const useCreateUser = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const createUser = async ({ firstName, lastName, username, password }) => {
+    setIsLoading(true);
+    setError(null);
+
+    return userService
+      .createUser({ firstName, lastName, username, password })
+      .then(() => navigate("/", { replace: true }))
+      .catch((err) => {
+        setError(err.response);
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  const resetError = () => setError(null);
+
+  return { createUser, isLoading, error, resetError };
+};
+
+export { useCreateUser };
