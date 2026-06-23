399d6bb8324c1f6d81b38a49ff67a5b2c97a0a3c
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-06-23 16:02:59 +0100

    feat: add authentication login, logout, status service funcs

---
+++ src/features/auth/services/authService.js
@@ -0,0 +1,24 @@
+import api from "../../../adapters/api.js";
+
+const basePath = "/auth";
+
+const login = async ({ username, password }) => {
+  const res = await api.post(`${basePath}/login`, {
+    username,
+    password,
+  });
+
+  return res;
+};
+
+const logout = async () => {
+  const res = await api.post(`${basePath}/logout`);
+  return res.data;
+};
+
+const status = async () => {
+  const res = await api.get(`${basePath}/status`);
+  return res;
+};
+
+export { login, logout, status };
