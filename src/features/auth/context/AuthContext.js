c5913be1a3cdea48bd0c764d3a32c73eae66a40a
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-06-23 16:42:46 +0100

    feat: add AuthContext to create context for AuthProvider values

---
+++ src/features/auth/context/AuthContext.js
@@ -0,0 +1,9 @@
+import { createContext, useContext } from "react";
+
+const AuthContext = createContext();
+
+const useAuthContext = () => {
+  return useContext(AuthContext);
+};
+
+export { AuthContext, useAuthContext };
