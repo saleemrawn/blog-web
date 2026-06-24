ed49dff7058d049a2e3db796d22588a0a5824fe1
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-06-24 15:33:15 +0100

    feat: add root export file for auth components, hooks and services

---
+++ src/features/auth/index.js
@@ -0,0 +1,6 @@
+export { SignupForm } from "../auth/components/SignupForm";
+export { LoginForm } from "../auth/components/LoginForm";
+export { AuthProvider } from "../auth/provider/AuthProvider";
+export { AuthContext, useAuthContext } from "../auth/context/AuthContext";
+export { useLogin, useLogout } from "../auth/hooks/useAuth";
+export { login, logout, status } from "../auth/services/authService";
