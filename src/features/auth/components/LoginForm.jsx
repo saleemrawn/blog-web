4ea34f8fc5a73b3a713de0f036a19cc3e9f7b028
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-06-24 15:54:24 +0100

    feat: add conditional render if user is already logged in

--- src/features/auth/components/LoginForm.jsx
+++ src/features/auth/components/LoginForm.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { Form } from "radix-ui";
-import { useLogin } from "../";
-import { Button, Flex, Heading, Spinner } from "@radix-ui/themes";
+import { useAuthContext, useLogin } from "../";
+import { Text, Button, Flex, Heading, Spinner, Box } from "@radix-ui/themes";
+import { Link } from "react-router";
 
 export const LoginForm = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const { isLoggedIn } = useAuthContext();
   const { login, isLoading, error, resetError } = useLogin();
 
   const serverError = error?.data?.message;
@@ -17,85 +19,94 @@ export const LoginForm = () => {
         Login
       </Heading>
 
-      <Form.Root
-        onSubmit={(event) => {
-          event.preventDefault();
-          login({ username, password });
-        }}
-      >
-        <Form.Field
-          className="FormField"
-          name="username"
-          serverInvalid={hasServerError}
+      {isLoggedIn ? (
+        <Box mt={"6"}>
+          <Text>
+            You are already logged in, and can return to{" "}
+            <Link to="/">homepage</Link>.
+          </Text>
+        </Box>
+      ) : (
+        <Form.Root
+          onSubmit={(event) => {
+            event.preventDefault();
+            login({ username, password });
+          }}
         >
-          <Flex direction={"column"} mb={"2"}>
-            <Form.Control asChild>
-              <input
-                type="text"
-                name="username"
-                id="username"
-                className="Input"
-                placeholder="Username"
-                value={username}
-                onChange={(e) => {
-                  resetError();
-                  setUsername(e.target.value);
-                }}
-                disabled={isLoading}
-                required
-              />
-            </Form.Control>
+          <Form.Field
+            className="FormField"
+            name="username"
+            serverInvalid={hasServerError}
+          >
+            <Flex direction={"column"} mb={"2"}>
+              <Form.Control asChild>
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  className="Input"
+                  placeholder="Username"
+                  value={username}
+                  onChange={(e) => {
+                    resetError();
+                    setUsername(e.target.value);
+                  }}
+                  disabled={isLoading}
+                  required
+                />
+              </Form.Control>
 
-            <Form.Message className="FormMessage" match="valueMissing">
-              Username is required
-            </Form.Message>
-          </Flex>
-        </Form.Field>
-
-        <Form.Field
-          className="FormField"
-          name="password"
-          serverInvalid={hasServerError}
-        >
-          <Flex direction={"column"} mb={"4"}>
-            <Form.Control asChild>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                className="Input"
-                placeholder="Password"
-                onChange={(e) => {
-                  resetError();
-                  setPassword(e.target.value);
-                }}
-                disabled={isLoading}
-                required
-              />
-            </Form.Control>
-
-            <Form.Message className="FormMessage" match="valueMissing">
-              Password is required
-            </Form.Message>
-
-            {serverError && (
-              <Form.Message
-                className="FormMessage"
-                match="badInput"
-                forceMatch={hasServerError}
-              >
-                {serverError}
+              <Form.Message className="FormMessage" match="valueMissing">
+                Username is required
               </Form.Message>
-            )}
-          </Flex>
-        </Form.Field>
+            </Flex>
+          </Form.Field>
 
-        <Form.Submit asChild>
-          <Button size={{ md: "3" }} color="gray" highContrast>
-            {isLoading ? <Spinner /> : "Login"}
-          </Button>
-        </Form.Submit>
-      </Form.Root>
+          <Form.Field
+            className="FormField"
+            name="password"
+            serverInvalid={hasServerError}
+          >
+            <Flex direction={"column"} mb={"4"}>
+              <Form.Control asChild>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  className="Input"
+                  placeholder="Password"
+                  onChange={(e) => {
+                    resetError();
+                    setPassword(e.target.value);
+                  }}
+                  disabled={isLoading}
+                  required
+                />
+              </Form.Control>
+
+              <Form.Message className="FormMessage" match="valueMissing">
+                Password is required
+              </Form.Message>
+
+              {serverError && (
+                <Form.Message
+                  className="FormMessage"
+                  match="badInput"
+                  forceMatch={hasServerError}
+                >
+                  {serverError}
+                </Form.Message>
+              )}
+            </Flex>
+          </Form.Field>
+
+          <Form.Submit asChild>
+            <Button size={{ md: "3" }} color="gray" highContrast>
+              {isLoading ? <Spinner /> : "Login"}
+            </Button>
+          </Form.Submit>
+        </Form.Root>
+      )}
     </>
   );
 };
