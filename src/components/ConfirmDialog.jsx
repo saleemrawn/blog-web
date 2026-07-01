5ad96ff60e45fe41132eababe0930990a4db06b5
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-07-01 10:57:22 +0100

    refactor: update button size for ConfirmDialog
    - add buttonSize prop to update size for trigger button
    - remove button sizes for dialog buttons
    - apply Prettier formatting

--- src/components/ConfirmDialog.jsx
+++ src/components/ConfirmDialog.jsx
@@ -1,25 +1,33 @@
 import { Button, AlertDialog, Flex } from "@radix-ui/themes";
 
-export const ConfirmDialog = ({ title, description, buttonText, onClick }) => {
+export const ConfirmDialog = ({
+  title,
+  description,
+  buttonText,
+  buttonSize,
+  onClick,
+}) => {
   return (
     <AlertDialog.Root>
       <AlertDialog.Trigger>
-        <Button variant="outline" size={{ md: "3" }} color="gray" highContrast>
+        <Button variant="outline" size={buttonSize} color="gray" highContrast>
           {buttonText}
         </Button>
       </AlertDialog.Trigger>
       <AlertDialog.Content maxWidth="450px">
         <AlertDialog.Title>{title}</AlertDialog.Title>
-        <AlertDialog.Description size="2">{description}</AlertDialog.Description>
+        <AlertDialog.Description size="2">
+          {description}
+        </AlertDialog.Description>
 
         <Flex gap="3" mt="4" justify="end">
           <AlertDialog.Cancel>
-            <Button variant="soft" color="gray" size={{ md: "3" }}>
+            <Button variant="soft" color="gray">
               Cancel
             </Button>
           </AlertDialog.Cancel>
           <AlertDialog.Action>
-            <Button onClick={onClick} variant="solid" size={{ md: "3" }} color="gray" highContrast>
+            <Button onClick={onClick} variant="solid" color="gray" highContrast>
               {buttonText}
             </Button>
           </AlertDialog.Action>
