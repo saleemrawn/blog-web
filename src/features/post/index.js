5c1e0925c7a454c56c13d4ede20fdfc0c08f50a3
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-08-07 14:10:56 +0100

    refactor: add PostDate to post root export index file

--- src/features/post/index.js
+++ src/features/post/index.js
@@ -2,3 +2,4 @@ export * from "../post/hooks/usePost";
 export { Post } from "./components/Post";
 export { PostList } from "./components/PostList";
 export { PostCategories } from "./components/PostCategories";
+export { PostDate } from "./components/PostDate";
