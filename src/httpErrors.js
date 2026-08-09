66ca4c7057a23c91d011adde3dad3f81a266d947
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-08-09 21:21:00 +0100

    refactor: simplify getErrorDetails to get error details obj
    - remove ERROR_CASES, and instead use error messages returned from server/API for improved maintainability
    - add conditionals based on error type e.g. response or request with fallbacks

--- src/httpErrors.js
+++ src/httpErrors.js
@@ -1,29 +1,29 @@
-const ERROR_CASES = {
-  400: { code: 400, message: "Bad Request" },
-  401: { code: 401, message: "Token expired. Please login again." },
-  403: { code: 403, message: "Unauthorised, permission denied." },
-  404: { code: 404, message: "Resource not found" },
-  500: { code: 500, message: "Internal Server Error. Please try again later." },
-  ERR_NETWORK: {
-    code: null,
-    message: "Network error. Please try again later.",
-  },
-  default: {
-    code: null,
-    message: "Oops, something went wrong. Please try again later.",
-  },
-};
-
 const getErrorDetails = (error) => {
-  if (error.request) {
-    return (
-      ERROR_CASES[error.code] ??
-      ERROR_CASES[error.status] ??
-      ERROR_CASES.default
-    );
+  if (error?.response) {
+    const code = error.response?.data?.code ?? error.response?.status ?? null;
+
+    const message =
+      error.response?.data?.message ??
+      error.response?.statusText ??
+      "Oops, something went wrong. Please try again later.";
+
+    const errors = error.response?.data?.errors ?? null;
+
+    return { code, message, errors };
   }
 
-  return ERROR_CASES[error.status] ?? ERROR_CASES.default;
+  if (error?.request) {
+    return {
+      code: null,
+      message: "Network error. Please try again later.",
+    };
+  }
+
+  return {
+    code: null,
+    message:
+      error?.message ?? "Oops, something went wrong. Please try again later.",
+  };
 };
 
 export { getErrorDetails };
