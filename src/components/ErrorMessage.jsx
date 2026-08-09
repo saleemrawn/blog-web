0db3c1f3e7c17cbafee6d9cdc7d973e4bfc2ffa1
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-08-09 21:25:17 +0100

    refactor: update error obj properties to match getErrorDetails obj

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,16 +1,15 @@
 import { Box, Flex, Text, Callout } from "@radix-ui/themes";
-import { getErrorDetails } from "../httpErrors";
 
-export const ErrorMessage = ({ error }) => {
+const ErrorMessage = ({ error }) => {
   if (!error) return null;
-
-  const err = getErrorDetails(error);
 
   return (
     <Callout.Root color="red" size={{ initial: "2", md: "3" }}>
       <Callout.Text>
-        {err?.code ? `${err?.code} -` : null} {err?.message}
+        {error?.message} {error?.code ? `(${error?.code})` : ""}
       </Callout.Text>
     </Callout.Root>
   );
 };
+
+export { ErrorMessage };
